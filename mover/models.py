a1e3bcfbc5d94a8a9e5f971ab90bd5b7a72b380a
Denis Parchenko <denis.parchenko@gmail.com>
2011-08-31 02:05:58 +0300

    Migrating models to Django

--- mover/models.py
+++ mover/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+	# This field is required.
+	user = models.OneToOneField(User)
+
+	# User names and avatars.
+	yandex_name = models.CharField(max_length=100)
+	yandex_picture = models.CharField(max_length=200)
+	google_name = models.CharField(max_length=100)
+	google_picture = models.CharField(max_length=200)
+
+	# Authentication tokens.
+	yandex_token = models.CharField(max_length=100)
+	yandex_secret = models.CharField(max_length=100)
+	google_token = models.CharField(max_length=100)
+	google_secret = models.CharField(max_length=100)
+
+class MoveArrangement(models.Model):
+	user = models.ForeignKey(User)
+
