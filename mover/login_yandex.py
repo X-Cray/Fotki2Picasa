e0ad886a7aa5ebbc9854dd8e69c11f62f7a2edcd
Denis Parchenko <denis.parchenko@gmail.com>
2011-09-09 01:41:58 +0300

    Removed an old file

--- mover/login_yandex.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import webapp
-from common import *
-
-class LoginYandex(webapp.RequestHandler):
-	def get(self):
-		self.redirect("https://oauth.yandex.ru/authorize?response_type=code&client_id=%s&state=yandex" % application_key_yandex)
